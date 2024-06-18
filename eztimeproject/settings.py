8e4c4594e196088377918a5bb6e0860430cff28f
Farah178 <farhana@ekfrazo.in>
2024-06-18 14:34:05 +0530

    Updated media issue

--- eztimeproject/settings.py
+++ eztimeproject/settings.py
@@ -212,7 +212,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'site', 'static')
 
 # Media files
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'site', 'media')
 
 
 
